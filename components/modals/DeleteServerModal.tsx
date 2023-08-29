36480f82b4d9be04a05509616d01afadb7758358
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-08-29 21:34:00 +0530

    delete server ⭕

--- components/modals/DeleteServerModal.tsx
+++ components/modals/DeleteServerModal.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "../ui/button";
@@ -15,58 +15,58 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 
 const DeleteServerModal = () => {
-    const { isOpen, onClose, type, data } = useModal();
-    const router = useRouter();
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
 
-    const isModalOpen = isOpen && type === "deleteServer";
+  const isModalOpen = isOpen && type === "deleteServer";
 
-    const { server } = data;
+  const { server } = data;
 
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const onClick = async () => {
-        try {
-            setIsLoading(true);
-            await axios.patch(`/api/servers/${server?.id}/leave`);
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/servers/${server?.id}`);
 
-            onClose();
-            router.refresh();
-            router.push("/");
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setIsLoading(false);
-        }
-    };
+      onClose();
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    return (
-        <Dialog open={isModalOpen} onOpenChange={onClose}>
-            <DialogContent className="bg-white text-black p-0 overflow-hidden">
-                <DialogHeader className="pt-8 px-6">
-                    <DialogTitle className="text-2xl text-center font-bold">
-                        Delete the server...
-                    </DialogTitle>
-                    <DialogDescription className="text-center text-zinc-500">
-                        Are you sure you want to delete{" "}
-                        <span className="font-semibold text-indigo-500">
-                            {server?.name}
-                        </span>
-                        ?
-                    </DialogDescription>
-                </DialogHeader>
-                <DialogFooter className="bg-gray-100 px-6 py-4">
-                    <div className="flex items-center justify-between w-full">
-                        <Button disabled={isLoading} onClick={onClose} variant="ghost">
-                            Cancel
-                        </Button>
-                        <Button disabled={isLoading} variant="primary" onClick={onClick}>
-                            Confirm
-                        </Button>
-                    </div>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    );
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Delete the server...
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want to do this? <br />
+            <span className="text-indigo-500 font-semibold">
+              {server?.name}
+            </span>{" "}
+            will be permanently deleted....
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-center justify-between w-full">
+            <Button disabled={isLoading} onClick={onClose} variant="ghost">
+              Cancel
+            </Button>
+            <Button disabled={isLoading} variant="primary" onClick={onClick}>
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default DeleteServerModal;
