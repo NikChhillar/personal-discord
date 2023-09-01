2a6ed6e08258abae395478e0d2b29ab8b3c2716a
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-01 12:18:50 +0530

    Live status 🚀

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 import UserAvatar from "../user-avatar";
 import MobileToggle from "../mobile-toggle";
+import SocketIndicator from "../socket-indicator";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -20,7 +21,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
         <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
-      <div className="ml-auto flex items-center"></div>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
