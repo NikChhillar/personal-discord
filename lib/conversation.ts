a86a6b97ca51f31387a4c0a4a541c5f48fdc6df8
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-01 11:24:13 +0530

    conversation started🚀

---
+++ lib/conversation.ts
@@ -0,0 +1,68 @@
+import { db } from "./db";
+
+export const getCreateConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  let conversation =
+    (await findConversation(memberOneId, memberTwoId)) ||
+    (await findConversation(memberTwoId, memberOneId));
+
+  if (!conversation) {
+    conversation = await createnewConversation(memberOneId, memberTwoId);
+  }
+
+  return conversation;
+};
+
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+  try {
+    return await db.conversation.findFirst({
+      where: {
+        AND: [{ memberOneId: memberOneId }, { memberTwoId: memberTwoId }],
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    return null;
+  }
+};
+
+const createnewConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  try {
+    return await db.conversation.create({
+      data: {
+        memberOneId,
+        memberTwoId,
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    return null;
+  }
+};
