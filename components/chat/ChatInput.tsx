db9df0d256c64e45288310302f27120bd66e92a3
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-02 21:03:04 +0530

    removed space before emoji in the message

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -75,15 +75,14 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                   <Input
                     disabled={isLoading}
                     className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
-                    placeholder={`Message ${
-                      type === "conversation" ? name : "#" + name
-                    }`}
+                    placeholder={`Message ${type === "conversation" ? name : "#" + name
+                      }`}
                     {...field}
                   />
                   <div className="absolute top-7 right-8">
                     <EmojiPicker
                       onChange={(emoji: string) =>
-                        field.onChange(`${field.value} ${emoji}`)
+                        field.onChange(`${field.value}${emoji}`)
                       }
                     />
                   </div>
